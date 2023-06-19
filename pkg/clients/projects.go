dd7d010bbda1b16b23d59d3f851402b36ea85fd5
Bassel Zeidan <bassel.zeidan@sap.com>
2023-06-19 17:36:06 +0200

    add project client

---
+++ pkg/clients/projects.go
@@ -0,0 +1,28 @@
+package clients
+
+import (
+	"github.com/gophercloud/gophercloud"
+	masterdata "github.com/sapcc/gophercloud-sapcc/billing/masterdata/projects"
+)
+
+type ProjectClient struct {
+	billingClient *gophercloud.ServiceClient
+}
+
+func NewProjectClient(billingClient *gophercloud.ServiceClient) (*ProjectClient) {
+	return &ProjectClient{
+		billingClient: billingClient,
+	}
+}
+
+func (pc *ProjectClient) GetProject(projectId string) (*masterdata.Project, error) {
+	projectMeta := masterdata.Get(pc.billingClient, projectId)
+	project, err := projectMeta.Extract()
+
+	if err != nil {
+		logger.Printf("failed to fetch project with id: %w", projectId)
+		return nil, err
+	}
+	logger.Printf("project with id: %s, has been fetched successfully", projectId)
+	return project, nil
+}
