1afa20cfbe800d33ca750579067672f5dd53a9a4
Bassel Zeidan <bassel.zeidan@sap.com>
2023-10-04 16:33:10 +0200

    update ignore file list

--- pkg/tests/command.sh
+++ pkg/tests/command.sh
@@ -2,10 +2,8 @@
 
 CURL='/usr/bin/curl'
 URL="http://127.0.0.1:8080/alerts"
-# HEADER1="User-Agent: Alertmanager/0.23.0"
-# HEADER2="Content-Type: application/json"
 REQ="POST"
 DATA="@alert.json"
-#-H $HEADER1 -H $HEADER2
+
 raw="$($CURL --request $REQ --data $DATA $URL)"
 echo $raw
